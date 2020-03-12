2028e801dae8dcf49d6181312ad12031b656970d
Antoine Lambert <antoine.lambert@inria.fr>
2020-03-12 19:17:14 +0100

    Fix C++11 support detection for Visual Studio compilers
    Simply checking on the value of the __cplusplus preprocessor variable
    when using MSVC is not reliable to detect C++11 support. Checking on
    the definition of _MSVC_LANG one is more robust as it was not the case
    before Visual Studio 2015 which is the first release to fully implement
    C++11 specifications.

--- source/utf8/checked.h
+++ source/utf8/checked.h
@@ -325,7 +325,7 @@ namespace utf8
 
 } // namespace utf8
 
-#if UTF_CPP_CPLUSPLUS >= 201103L // C++ 11 or later
+#if UTF_CPP_CPLUSPLUS >= 201103L || defined(_MSVC_LANG) // C++ 11 or later
 #include "cpp11.h"
 #endif // C++ 11 or later
 
